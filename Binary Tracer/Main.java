65ff8feb47bcad94d70e08791990340ebf85ecb2
Anthony.Yarosh <Anthony.Yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2014-12-16 09:00:52 -0700

    Commit 1

---
+++ Binary Tracer/Main.java
@@ -0,0 +1,49 @@
+import java.awt.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main
+	{
+	static String name;
+	static int elements;
+	static int target = 1;
+
+	public static void main(String[] args)
+		{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome Stranger, Please Enter Your Name");
+		name = input.nextLine();
+		System.out.println("Welcome to learning binary tracing " + name + "\n");
+		System.out
+				.println("In the line, please enter an array length 3-10, preferably, however you may go higher.");
+		ArrayList e = new ArrayList();
+		System.out.println("Enter your ArrayList.");
+		String a = input.nextLine();
+		e.add(a);
+		System.out.println(e);
+		
+		// binarySearch(null, elements);
+		}
+
+	public static void binarySearch(int[] e, int target)
+		{
+		int left = 0;
+		int right = e.length;
+		while (left <= right)
+			{
+			int middle = (left + right) / 2;
+			if (target < e[middle])
+				{
+				right = middle - 1;
+				} else if (target > e[middle])
+				{
+				left = middle + 1;
+				} else
+				{
+				System.out.println(middle);
+				}
+			}
+		System.out.println("-1");
+		}
+	
+	}
